ee0f22b67bd0ee71d69a23e2e7b6b83b7baf56f5
daytoysss <szeying12@hotmail.com>
2021-07-25 17:15:13 +0700

    add login

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/RootStackParams';
 import Home from './Home';
+import Login from '../screens/Login';
+import { useAppSelector } from '../redux-toolkit/hook';
 const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function AppRoute() {
+  const loggedIn = useAppSelector(state => state.auth.authenticated);
   return (
     <RootStack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      <RootStack.Screen name="HomeStack" component={Home} />
+      {loggedIn ? (
+        <RootStack.Screen name="HomeStack" component={Home} />
+      ) : (
+        <RootStack.Screen name="Login" component={Login} />
+      )}
     </RootStack.Navigator>
   );
 }
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,18 +1,87 @@
+import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+} from 'react-native';
+import { setLoginState } from '../../redux-toolkit/authSlice';
+import { useAppDispatch } from '../../redux-toolkit/hook';
+import { RootStackParamList } from '../../types/RootStackParams';
+import { colors } from '../../utils/constants';
+
+type LoginPropType = StackNavigationProp<RootStackParamList, 'Login'>;
 
 type Props = {
-  navigation: undefined;
+  navigation: LoginPropType;
 };
 
 const Screen: React.FC<Props> = ({ navigation }) => {
+  const dispatch = useAppDispatch();
+  const handleLogin = async () => {
+    dispatch(setLoginState(true));
+  };
+
   return (
-    <SafeAreaView>
-      <TouchableOpacity>
-        <Text>asd</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome to</Text>
+      <Text style={styles.title}>ClassLink</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.label}>Password</Text>
+        <TextInput style={styles.input} />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginText}>Login</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 50,
+  },
+  title: {
+    fontSize: 50,
+    color: colors.title,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  inputContainer: {
+    alignSelf: 'flex-start',
+  },
+  input: {
+    borderColor: colors.black,
+    borderWidth: 1,
+    width: Dimensions.get('screen').width - 100,
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  loginBtn: {
+    marginTop: 20,
+    alignSelf: 'flex-end',
+    height: 50,
+    backgroundColor: colors.buttonLoginBgc,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+  },
+  loginText: {
+    color: colors.white,
+  },
+});
 
 export default Screen;
