6a4ec1f9af6bd6df55321c0962b652f7a7633845
daytoysss <szeying12@hotmail.com>
2021-07-25 22:09:04 +0700

    add report

---
+++ src/screens/Report/Report.tsx
@@ -0,0 +1,96 @@
+import { useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+} from 'react-native';
+import Header from '../../components/Header';
+import {
+  HomeStackParamsList,
+  HomeStackRouteProps,
+} from '../../types/HomeParamsList';
+import { colors, ScreenWidth } from '../../utils/constants';
+import ProgressCircle from 'react-native-progress/Pie';
+
+type CreateClassPropType = StackNavigationProp<
+  HomeStackParamsList,
+  'CreateClass'
+>;
+
+type Props = {
+  navigation: CreateClassPropType;
+};
+
+const Screen: React.FC<Props> = ({ navigation }) => {
+  const route = useRoute<HomeStackRouteProps<'Report'>>();
+  const studentName = route.params.name;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header
+        navigation={navigation}
+        title={`${studentName}'s report`}
+        isBackable={true}
+      />
+      <View style={styles.body}>
+        <ProgressCircle
+          unfilledColor={colors.incompleted}
+          color={colors.completed}
+          textStyle={'white'}
+          size={ScreenWidth / 2}
+          progress={0.6}
+        />
+      </View>
+      <View style={styles.percentageBlock}>
+        <View style={styles.percentageRow}>
+          <Text style={styles.textCompleted}>Work completed</Text>
+          <Text style={styles.textCompleted}>60%</Text>
+        </View>
+        <View style={styles.percentageRow}>
+          <Text style={styles.textNotCompleted}>Work not completed</Text>
+          <Text style={styles.textNotCompleted}>40%</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  body: {
+    marginTop: 50,
+    marginHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  percentageBlock: {
+    marginTop: 50,
+    marginHorizontal: 50,
+  },
+  percentageRow: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textCompleted: {
+    color: colors.completed,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  textNotCompleted: {
+    color: colors.incompleted,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+});
+
+export default Screen;
