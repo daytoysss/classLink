6c223cafd475f8952e4c8c80eaa99f7e80950ad5
daytoysss <szeying12@hotmail.com>
2021-07-25 17:15:55 +0700

    add constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,10 @@ export const colors = {
   title: '#cc0065',
   white: '#fff',
   black: '#000',
-  buttonBgc: '#fb7299',
+  buttonNewClass: '#fb7299',
+  homeBgc: '#f2da94',
+  classroomBgc: '#c882ea',
+  bookingBgc: '#c5e181',
+  summaryBgc: '#f27177',
+  buttonLoginBgc: '#6699cc',
 };
