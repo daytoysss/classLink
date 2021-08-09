03eb9ebf005d214a3c5a5ccc72ae6617b0754ccf
“daytoysss” <szeying12@hotmail.com>
2021-08-10 00:42:19 +0700

    fix home due to role

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,26 @@
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { DrawerActions, useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  Keyboard,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import Header from '../../components/Header';
-import { HomeStackParamsList } from '../../types/HomeParamsList';
+import { useAppSelector } from '../../redux-toolkit/hook';
 import { TabParams } from '../../types/TabParams';
-import { colors } from '../../utils/constants';
+import { baseURL, colors } from '../../utils/constants';
 
 type HomeStackProps = BottomTabNavigationProp<TabParams, 'Home'>;
 
@@ -16,19 +30,176 @@ type Props = {
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const RootNavigation = useNavigation();
+  const role = useAppSelector(state => state.role.role);
+  const userInfor = useAppSelector(state => state.user.info);
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleCreatePost = async () => {
+    if (!title || !content) {
+      Alert.alert('ClassLink', 'Both title and content are required!');
+    } else {
+      try {
+        const res = await axios.post(baseURL + 'posts', {
+          user_id: userInfor.user_id ?? '',
+          post_title: title,
+          post_content: content,
+        });
+        if (res.data.status !== 'success') {
+          Alert.alert('ClassLink', res.data.message);
+        } else {
+          setLoading(true);
+          setTitle('');
+          setContent('');
+          await getData();
+        }
+      } catch (err) {
+        Alert.alert('ClassLink', JSON.stringify(err.response));
+      }
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(baseURL + 'posts');
+      console.log(res.data);
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Header navigation={navigation} title="ClassLink" isBackable={false}/>
-      <TouchableOpacity
-        style={styles.buttonCreate}
-        onPress={() =>
-          RootNavigation.navigate('Main', {
-            screen: 'CreateClass',
-          })
-        }>
-        <Text style={[styles.text, { fontSize: 80 }]}>+</Text>
-        <Text style={[styles.text, { fontSize: 40 }]}>New class</Text>
-      </TouchableOpacity>
+      <Header navigation={navigation} title="ClassLink" isBackable={false} />
+      {role === 'teacher' && (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View
+            style={{
+              height: 200,
+              backgroundColor: colors.white,
+              borderColor: colors.black,
+              borderRadius: 20,
+              borderWidth: 0.5,
+              margin: 10,
+            }}>
+            <TextInput
+              placeholder="Title.."
+              value={title}
+              onChangeText={setTitle}
+              style={{
+                fontSize: 20,
+                paddingLeft: 20,
+                paddingTop: 20,
+                height: 50,
+                borderBottomWidth: 0.5,
+                borderColor: colors.black,
+              }}
+            />
+            <TextInput
+              value={content}
+              onChangeText={setContent}
+              placeholder="Content.."
+              textAlignVertical="top"
+              multiline={true}
+              numberOfLines={4}
+              style={{
+                fontSize: 20,
+                paddingLeft: 20,
+                paddingTop: 20,
+                height: 100,
+                borderBottomWidth: 0.5,
+                borderColor: colors.black,
+              }}
+            />
+            <View
+              style={{
+                height: 50,
+                paddingHorizontal: 30,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  Keyboard.dismiss();
+                }}>
+                <Text>Add</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleCreatePost}>
+                <Text>Post</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          alignSelf: 'center',
+          marginVertical: 10,
+          height: 40,
+        }}>
+        Incoming events
+      </Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="black" />
+      ) : (
+        <>
+          {posts.length === 0 ? (
+            <Text>No posts!</Text>
+          ) : (
+            <FlatList
+              keyExtractor={item => item?.id ?? ''}
+              data={posts}
+              extraData={posts}
+              renderItem={({ item }: any) => {
+                console.log(item);
+                return (
+                  <TouchableOpacity
+                    onPress={() =>
+                      RootNavigation.navigate('EventDetail', { item })
+                    }
+                    // key={post.post_id}
+                    style={{
+                      backgroundColor: colors.white,
+                      borderRadius: 10,
+                      padding: 20,
+                      marginTop: 20,
+                    }}>
+                    <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+                      {item?.post_title}
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                        fontStyle: 'italic',
+                      }}>
+                      {new Date(item?.created_at).toDateString().slice(4, 10)}
+                    </Text>
+                    <Text>{item?.post_content}</Text>
+                  </TouchableOpacity>
+                );
+              }}
+              contentContainerStyle={{
+                flexGrow: 1,
+                marginHorizontal: 20,
+                paddingBottom: 150,
+              }}
+            />
+          )}
+        </>
+      )}
     </SafeAreaView>
   );
 };
